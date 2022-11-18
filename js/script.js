ddc74c46a7900989d70ce17ee30fa33d10939d79
Umit-Ozdemir <118529752+Umit-Ozdemir@users.noreply.github.com>
2022-11-18 14:25:37 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+$(".txtb").on("keyup", function(e){
+    // Die Zahl 13 steht für die Enter Taste
+    if (e.keyCode == 13 && $(".txtb").val() != "")
+
+    {
+        var task = $("<div class='task'></div>").text($(".txtb").val());
+
+        var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
+            var p = $(this).parent();
+            p.fadeOut(function(){
+                p.remove();
+            });
+        });
+        var check = $("<i class='fas fa-check'></i>").click(function(){
+            var p = $(this).parent();
+            p.fadeOut(function(){
+                $(".comp").append(p);
+                p.fadeIn()
+            });
+            $(this).remove();
+        });
+
+        $(".notcomp").append(task);
+        // Eingabefeld leeren
+        $(".txtb").val("");
+    }
+
+})
