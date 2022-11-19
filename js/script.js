b4b8134ae080defacc9afbd8e0865e588aad72f0
MazenHetini <86821991+MazenHetini@users.noreply.github.com>
2022-11-19 15:55:33 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-/* function getSuggestions() {
-    // JSON-File mit AJAX laden
-    let xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        if (xhr.status != 200) return;
-
-        let jsonParsed;
-
-        if (xhr.responseType == "json") jsonParsed = xhr.response;
-        else jsonParsed = JSON.parse(xhr.responseText);
-        for (let i = 0; i < jsonParsed.length; i++) {
-            let suggestion = "<p>" + jsonParsed[i] + "</p>";
-            $(".suggestions").append(suggestion);
-        }
-    };
-    xhr.open("GET", "JSON/vorschlag.json");
-    xhr.responseType = "json";
-    xhr.send();
-}
-getSuggestions(); */
-
 $.ajax({
     url: 'JSON/vorschlag.json',
     dataType: 'json',
